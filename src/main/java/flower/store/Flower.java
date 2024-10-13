0f71d0d0eb030e6d89ef944730002858c772338b
Korch195 <korchevyi.pn@ucu.edu.ua>
2024-10-13 14:34:47 +0300

    first commit

---
+++ src/main/java/flower/store/Flower.java
@@ -0,0 +1,28 @@
+package flower.store;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Flower {
+    @Getter
+    private double sepalLength;
+    private FlowerColor color;
+    @Getter
+    private double price;
+    @Getter
+    private FlowerType flowerType;
+
+    public Flower(Flower flower) {
+        this.color = flower.color;
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+    }
+    public String getColor() {
+        return color.toString();
+    }
+}
---
+++ src/main/java/flower/store/FlowerPack.java
@@ -0,0 +1,17 @@
+package flower.store;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter @Setter @NoArgsConstructor
+public class FlowerPack {
+    private Flower flower;
+    private int quantity;
+    public FlowerPack(Flower flower, int quantity) {
+        this.flower = new Flower(flower);
+        this.quantity = quantity;
+    }
+    public double getPrice(){
+        return flower.getPrice()*quantity;
+    }
+}
